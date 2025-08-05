a6d007dbd2939ec855a65b7135f588074ff17abf
山口絃 <gen.yamaguchi@exc.tokai-rika.co.jp>
2025-08-05 16:52:44 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export { AiResearchIdpProvider } from "./AiResearchIdpProvider";
 export { signOutRedirect, type Profile } from "./utils";
+
+// このパッケージを使う側で react-oidc-context から useAuth をインポートすると
+// "AuthProvider の子コンポーネントで useAuth を使用する必要があります" というエラーが出るため
+// useAuth を再エクスポートする。これで回避できる（ちょっと謎だが）
 export { useAuth } from "react-oidc-context";
